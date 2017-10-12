913e53857d774ae6e051251fe65c631cadca7897
Oscar Ricaud <oiricaud@miners.utep.edu>
2017-10-11 18:57:28 -0600

    rearranging the table to be at the bottom of the tasks

--- my-carwheel-app/src/app/app.module.ts
+++ my-carwheel-app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MyTableComponent } from './my-table/my-table.component';
 import {MatTableModule} from '@angular/material';
 import {MatGridListModule} from '@angular/material';
 import {MatCardModule} from '@angular/material';
+import {MatFormFieldModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatCardModule} from '@angular/material';
     MatButtonModule,
     MatTableModule,
     MatGridListModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule
 
   ],
   providers: [],
