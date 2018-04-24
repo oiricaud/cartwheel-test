44e68c394b6ff1b770f9938c35c2587ef4d93fa0
oiricaud <oiricaud@miners.utep.edu>
2018-04-23 20:46:57 -0600

    updated system to use karma isntanbul a coverage tool that analyzes the code

--- my-carwheel-app/src/environments/environment.prod.ts
+++ my-carwheel-app/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCOAOVjlb23Un3UWFeXmSmBi4wuC5UE-gA',
+    authDomain: 'cartwheel-1d3df.firebaseapp.com',
+    databaseURL: 'https://cartwheel-1d3df.firebaseio.com',
+    storageBucket: 'cartwheel-1d3df.appspot.com',
+    messagingSenderId: '278786016821'
+  }
 };
